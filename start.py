8c9010704540d58af5f59a5e4abad573c9773a9f
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-11 09:55:06 +0530

    Done COPYRIGHT TEAM DYNAMIC

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
+system ("git clone https://GitHub.com/GODBOYX/DARKLON && cd DARKLON && python3 -m userbot")
 # try af
 print("There Is A Problem In Userbot")
-system("python3 -m DYNAMIC")
-print ("You Have Error In Your Userbot Check If You Are A Owner Or Dev OR Join For Support @DYNAMICUSERBOTSUPPORT")
+system("python3 -m userbot")
+print ("You Have Error In Your Userbot Check error If You Are A Owner Or Dev. If Cant Fix Contact Aman Pandey ft @OFFLINEMODEON OR Join For Support @DARKLON_USERBOT_SUPPORT_CHAT)
