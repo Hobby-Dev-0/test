a42ff24769f7238bd59ea73effdbab0a44b2d614
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-07 17:07:21 +0530

    Ok

--- start.py
+++ start.py
@@ -1,6 +1,3 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/GODBOYX/DARKLON && cd DARKLON && python3 -m userbot")
-# try af
-print("There Is A Problem In Userbot")
+system ("git clone https://GitHub.com/TeamExtremePro/ExtremeProUserbot && cd ExtremeProUserbot && python3 -m userbot")
 system("python3 -m userbot")
-print ("You Have Error In Your Userbot Check error If You Are A Owner Or Dev. If Cant Fix Contact Aman Pandey ft @OFFLINEMODEON OR Join For Support @DARKLON_USERBOT_SUPPORT_CHAT)
