e51a92690a6fa14d5a6602057087b5d49ab4185d
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-04 12:06:39 +0530

    F

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/TeamExtremePro/ExtremeProUserbot && cd ExtremeProUserbot && python3 -m userbot")
-system("python3 -m userbot")
+system ("pip install--upgrade Extre")
+system ("git clone https://GitHub.com/TeamExtremePro/ExtremeProUserbot && cd ExtremeProUserbot && python3 -m Extre")
+system("python3 -m Extre")
